ecbd595814343d4b3e9185d669eade9587782f14
wayne <lightww@me.com>
2017-01-17 14:14:51 +0800

    implement job CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "本SEED 可以自动创建一个ADMIN账号，并产生10个PUBLIC jobs ，10个 hidden jobs"
+
+create_account = User.create([email: 'example@email.com', password: '12345678', password_confirmation: '12345678'])
+puts "admin account created"
+
+create_jos = for i in 1..10 do
+Job.create!([title: "public-job no. #{i}", description: "这是 seed 创建的第 #{i} 个 public-job"])
+end
+puts "public-jobs created"
+
+create_jos= for i in 1..10 do
+  Job.create!([title: "hidden-job no. #{i}", description: "这是 seed 创建的第 #{i} 个 hidden-job"])
+end
+puts "hidden-job created"
