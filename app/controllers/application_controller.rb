81d5db0c08f3629c62eee29bbdac8b42ffc037d5
wayne <lightww@me.com>
2017-01-25 09:56:59 +0800

    better require_is_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  def require_is_admin
-    if !current_user.is_admin?
-      flash[:alert]="You are not admin"
-      redirect_to jobs_path
-
+    protect_from_forgery with: :exception
+    def require_is_admin
+        unless current_user && current_user.is_admin?
+            flash[:alert] = 'You are not admin'
+            redirect_to jobs_path
+        end
     end
-  end
 end
